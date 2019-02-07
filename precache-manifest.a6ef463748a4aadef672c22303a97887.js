3e5dbfdba4e59b270c2f3bc4b52e3d9b70f0c935
Apollo21552 <41828188+Apollo21552@users.noreply.github.com>
2019-02-06 20:52:05 -0500

    Updates

---
+++ precache-manifest.a6ef463748a4aadef672c22303a97887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6a95ee022bc8ade81cd",
+    "url": "/face-recognition-brain/static/js/runtime~main.b6a95ee0.js"
+  },
+  {
+    "revision": "4e33af757dd857bb2592",
+    "url": "/face-recognition-brain/static/js/main.4e33af75.chunk.js"
+  },
+  {
+    "revision": "fb8f8f48ecf253eb26d1",
+    "url": "/face-recognition-brain/static/js/1.fb8f8f48.chunk.js"
+  },
+  {
+    "revision": "4e33af757dd857bb2592",
+    "url": "/face-recognition-brain/static/css/main.2139d169.chunk.css"
+  },
+  {
+    "revision": "fb8f8f48ecf253eb26d1",
+    "url": "/face-recognition-brain/static/css/1.81954950.chunk.css"
+  },
+  {
+    "revision": "14c6d0a2cc4e195f0bc6a90b178a6cd5",
+    "url": "/face-recognition-brain/index.html"
+  }
+];
